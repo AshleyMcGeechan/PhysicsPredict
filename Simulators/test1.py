70b1d85e6dc45d2e9e1b87647fdce8468af022f6
Ashley McGeechan <apmcgeechan@gmail.com>
2018-09-24 21:30:06 +0100

    pyBox2d experimentation

---
+++ Simulators/test1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .framework import (Framework, Keys, main)
+from Box2D import (b2CircleShape, b2FixtureDef, b2LoopShape, b2PolygonShape,
+                   b2RevoluteJointDef, b2_pi)
+
+
+class Test1(Framework):
+    name = "Test1"
+    description = "Initial experimentation with pyBox2D"
+    bodies = []
+    joints = []
+
+    def __init__(self):
+        super(Test1, self).__init__()
+
+        self.world.gravity = (0, 0)
+
+        table = self.world.CreateBody(
+            shapes=b2LoopShape(vertices=[(0, 0), (0, 40),
+                                         (20, 40), (20, 0),
+                                         ]),
+        )
+
+        self.ball = self.world.CreateDynamicBody(
+            fixtures=b2FixtureDef(
+                shape=b2CircleShape(radius=1),
+                density=1.0,
+                friction=5),
+            angularDamping=0.5,
+            bullet=True,
+            position=(5, 10))
+
+        self.ball = self.world.CreateDynamicBody(
+            fixtures=b2FixtureDef(
+                shape=b2CircleShape(radius=1),
+                density=1.0,
+                friction=5),
+            angularDamping=0.5,
+            bullet=True,
+            position=(5, 30))
+
+
+if __name__ == "__main__":
+    main(Test1)
